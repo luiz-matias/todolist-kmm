4c71716d7df1975fe8cbfe5e097d75f7a865846a
Luiz Matias <luiz.matias1999@gmail.com>
2021-12-12 02:57:03 -0300

    FEAT: Prepared KMM and Android's First Screen

--- androidApp/src/main/java/com/luizmatias/todoapp/android/LoginActivity.kt
+++ androidApp/src/main/java/com/luizmatias/todoapp/android/LoginActivity.kt
@@ -9,7 +9,7 @@ fun greet(): String {
     return Greeting().greeting()
 }
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
